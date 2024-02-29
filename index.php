2212e3ea3de7d25ffc08b8bde9b9e3e7861a7c06
Sara Maraghelli <maraghellisara@gmail.com>
2024-02-29 21:15:46 +0100

    esercizio finito

---
+++ index.php
@@ -0,0 +1,74 @@
+<?
+//Traccia 1
+// $users = [
+//     [
+//         'name' => 'Davide',
+//         'surname' => 'Cariola',
+//         'gender' => 'male',
+//     ],
+
+//     [
+//         'name' => 'Sara',
+//         'surname' => 'Maraghelli',
+//         'gender' => 'female'
+//     ],
+
+//     [
+//         'name' => 'Antonio',
+//         'surname' => 'Rossi',
+//         'gender' => 'male'
+//     ],
+
+//     [
+//         'name' => 'Giulia',
+//         'surname' => 'Bianchi',
+//         'gender' => 'female'
+//     ],
+// ];
+
+// for ($i = 0; $i < count($users); $i++) {
+//     if ($users[$i]['gender'] === 'female') {
+//         echo 'Buongiorno signora ' . $users[$i]['name'] ." ". $users[$i]['surname']. "\n";
+//     } else {
+//         echo 'Buongiorno signor ' . $users[$i]['name'] ." ". $users[$i]['surname']. "\n";
+//     }
+// }
+
+//Traccia 2
+
+// $array = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+// $sommaNumeriPari = 0;
+// $numeriPari = 0;
+
+// for ($i = 0; $i < count($array); $i++) {
+//     if ($array[$i] % 2 === 0) {
+//         $sommaNumeriPari += $array[$i];
+//         $numeriPari++;
+//     }
+
+// };
+// if ($numeriPari > 0) {
+//     $media = $sommaNumeriPari / $numeriPari;
+//     echo 'La media dei numeri pari è ' . $media;
+// };
+
+
+//Traccia 3
+
+// for ($i = 0; $i <= 100; $i++) {
+
+//     if ($i % 3 === 0 && $i % 5 === 0) {
+//         echo 'PHP'. "\n";
+//     } else if ($i % 5 === 0) {
+//         echo 'JAVASCRIPT'. "\n";
+//     } else if ($i % 3 === 0) {
+//         echo 'HACKADEMY'. "\n";
+//     }else{
+//         echo $i."\n";
+//     }
+// };
+
+
+
+
+
